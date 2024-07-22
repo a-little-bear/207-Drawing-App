d4464416f914cd0cd9481e17c68060f81c20b22e
Ibrahim-Siddique <139663719+Ibrahim-Siddique@users.noreply.github.com>
2024-07-21 23:51:43 -0400

    Create more tests

---
+++ Painter Project/test/ControllerTest.java
@@ -0,0 +1,35 @@
+import main.View;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import controllers.Controller;
+
+public class ControllerTest {
+    private Controller controller;
+    private View view;
+
+    @Before
+    public void setUp() {
+        view = mock(View.class);
+        controller = new Controller(view);
+    }
+
+    @Test
+    public void testControllerInitialization() {
+        assertNotNull(controller.mouse);
+        assertNotNull(controller.keyboard);
+    }
+
+    @Test
+    public void testMouseIntegration() {
+        assertNotNull(controller.mouse);
+    }
+
+    @Test
+    public void testKeyboardIntegration() {
+        assertNotNull(controller.keyboard);
+    }
+}
