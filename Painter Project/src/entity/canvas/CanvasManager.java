569742033207d31132802993ab369abeb30b9f7d
Aeripsen <sepehr1381ja@gmail.com>
2024-07-22 14:43:14 -0400

    added getter to CanvasManager for testing

--- Painter Project/src/entity/canvas/CanvasManager.java
+++ Painter Project/src/entity/canvas/CanvasManager.java
@@ -86,4 +86,8 @@ public class CanvasManager {
         frame.setLocationRelativeTo(null); // Center the frame on the screen
         frame.setVisible(true);
     }
+    // Getter for layers
+    public ArrayList<Canvas> getLayers() {
+        return layers;
+    }
 }
