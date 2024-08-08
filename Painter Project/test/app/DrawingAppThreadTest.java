639c88ca8489b48da72b2a50cf6fdb62c6bd3951
Aeripsen <sepehr1381ja@gmail.com>
2024-08-08 14:38:27 -0400

    api tests

--- Painter Project/test/app/DrawingAppThreadTest.java
+++ Painter Project/test/app/DrawingAppThreadTest.java
@@ -1,13 +1,12 @@
 //package app;
 //
 //import interface_adapter.Controller;
-//import use_case.OutputBoundary;
-//import view.View;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.Test;
 //import org.mockito.Mockito;
+//import use_case.OutputBoundary;
+//import view.View;
 //
-//import static org.junit.jupiter.api.Assertions.assertFalse;
 //import static org.junit.jupiter.api.Assertions.assertTrue;
 //import static org.mockito.Mockito.mock;
 //
@@ -16,29 +15,22 @@
 //    private View mockView;
 //    private Controller mockController;
 //    private OutputBoundary mockOutputBoundary;
-//    private Thread mockThread;
 //
 //    @BeforeEach
 //    void setUp() {
 //        mockView = mock(View.class);
 //        mockController = mock(Controller.class);
 //        mockOutputBoundary = mock(OutputBoundary.class);
-//        mockThread = Mockito.spy(new Thread(() -> {}));
-//        drawingAppThread = new DrawingAppThread(mockView, mockController, mockOutputBoundary, mockThread);
+//        drawingAppThread = new DrawingAppThread(mockView, mockController, mockOutputBoundary);
 //    }
 //
 //    @Test
-//    void testThreadLifecycle() throws InterruptedException {
-//        drawingAppThread.startThread();
-//        Mockito.verify(mockThread).start();
+//    void testThreadStarts() throws InterruptedException {
+//        Thread thread = drawingAppThread.getThread();
+//        assertTrue(thread.isAlive(), "Thread should be alive after starting");
 //
-//        mockThread.start();
-//        Thread.sleep(50); // Allow some time for the thread to run
-//
-//        assertTrue(mockThread.isAlive());
-//        mockThread.interrupt();
-//        Thread.sleep(50); // Allow some time for the interrupt to process
-//
-//        assertFalse(mockThread.isAlive() || mockThread.isInterrupted());
+//        thread.interrupt();
+//        thread.join(50); // Allow some time for the interrupt to process
+//        assertTrue(thread.isInterrupted(), "Thread should be interrupted");
 //    }
 //}
