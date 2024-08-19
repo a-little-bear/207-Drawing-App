3619aea6d83b79c798dad4102c44959bc7fdf46e
sanchitm25 <sanchit550s.m@gmail.com>
2024-08-19 15:46:32 -0400

    Updated Serilization and Deserialization

--- Painter Project/src/toolbar/button/action_button/ExportCanvasActionButton.java
+++ Painter Project/src/toolbar/button/action_button/ExportCanvasActionButton.java
@@ -5,12 +5,12 @@ import java.awt.event.KeyEvent;
 
 public class ExportCanvasActionButton extends AbstractActionButton {
     private Icon icon;
-    private final static String iconPath = "";
+    private final static String iconPath = "Painter Project/images/ExportCanvasActionButton.png";
     private final static String toolTip = "Export Canvas (E)";
 
     public ExportCanvasActionButton(Action listener) {
         super(iconPath, toolTip, listener);
-        getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_C, 0), "e");
+        getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_E, 0), "e");
         getActionMap().put("e", listener);
     }
 }
--- Painter Project/src/toolbar/button/action_button/ImportCanvasActionButton.java
+++ Painter Project/src/toolbar/button/action_button/ImportCanvasActionButton.java
@@ -5,12 +5,12 @@ import java.awt.event.KeyEvent;
 
 public class ImportCanvasActionButton extends AbstractActionButton {
     private Icon icon;
-    private final static String iconPath = "";
+    private final static String iconPath = "Painter Project/images/ImportCanvasActionButton.png";
     private final static String toolTip = "Import Canvas (I)";
 
     public ImportCanvasActionButton(Action listener) {
         super(iconPath, toolTip, listener);
-        getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_C, 0), "i");
+        getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_I, 0), "i");
         getActionMap().put("i", listener);
     }
 }
