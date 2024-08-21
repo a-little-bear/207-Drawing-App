6091f5a5b2aa875bd41b81722d0697fbab64a2d4
Aeripsen <sepehr1381ja@gmail.com>
2024-08-21 01:01:12 -0400

    test

--- Painter Project/test/entity/canvas/CanvasTest.java
+++ Painter Project/test/entity/canvas/CanvasTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -59,5 +60,73 @@ public class CanvasTest {
         canvas.draw(g2);
         g2.dispose();
         assertNotNull(img, "Image should not be null after drawing");
+    }
+
+    @Test
+    public void testSerialization() throws IOException, ClassNotFoundException {
+        // Paint something on the canvas to ensure there's content to serialize
+        canvas.paintArea(10, 10, Color.BLUE, 20);
+
+        // Serialize the canvas
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(canvas);
+        oos.close();
+
+        // Deserialize the canvas
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        Canvas deserializedCanvas = (Canvas) ois.readObject();
+        ois.close();
+
+        // Validate that the deserialized canvas has the same properties as the original
+        assertNotNull(deserializedCanvas, "Deserialized canvas should not be null");
+        BufferedImage originalImage = canvas.getCanvasImage();
+        BufferedImage deserializedImage = deserializedCanvas.getCanvasImage();
+
+        assertEquals(originalImage.getWidth(), deserializedImage.getWidth(), "Width should match after deserialization");
+        assertEquals(originalImage.getHeight(), deserializedImage.getHeight(), "Height should match after deserialization");
+
+        // Check that the content is the same
+        for (int x = 0; x < originalImage.getWidth(); x++) {
+            for (int y = 0; y < originalImage.getHeight(); y++) {
+                assertEquals(originalImage.getRGB(x, y), deserializedImage.getRGB(x, y),
+                        "Pixel content should match after deserialization");
+            }
+        }
+    }
+
+    @Test
+    public void testSerializationEmptyCanvas() throws IOException, ClassNotFoundException {
+        // Ensure the canvas is clear
+        canvas.clearCanvas();
+
+        // Serialize the canvas
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(canvas);
+        oos.close();
+
+        // Deserialize the canvas
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        Canvas deserializedCanvas = (Canvas) ois.readObject();
+        ois.close();
+
+        // Validate that the deserialized canvas has the same properties as the original
+        assertNotNull(deserializedCanvas, "Deserialized canvas should not be null");
+        BufferedImage originalImage = canvas.getCanvasImage();
+        BufferedImage deserializedImage = deserializedCanvas.getCanvasImage();
+
+        assertEquals(originalImage.getWidth(), deserializedImage.getWidth(), "Width should match after deserialization");
+        assertEquals(originalImage.getHeight(), deserializedImage.getHeight(), "Height should match after deserialization");
+
+        // Check that the content is the same
+        for (int x = 0; x < originalImage.getWidth(); x++) {
+            for (int y = 0; y < originalImage.getHeight(); y++) {
+                assertEquals(originalImage.getRGB(x, y), deserializedImage.getRGB(x, y),
+                        "Pixel content should match after deserialization");
+            }
+        }
     }
 }
